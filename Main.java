d8bf27c780473e1fc1db5b0e0fcc6b7fe0e52530
hospital <calculusstepsign@gmail.com>
2026-02-04 21:16:12 +0500

    gg

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+package model;
 import model.menu.Menu;
 import model.menu.HospitalMenu;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 
 public class Main {
     public static void main(String[] args) {
